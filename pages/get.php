8b8f27ceee4e284eafa2b347195c636d34eb5159
A35G <a35g@hackworld.it>
2013-10-17 23:28:32 +0200

    initial project version

---
+++ pages/get.php
@@ -0,0 +1,37 @@
+<?php
+
+  function randPass ( $upper = 3, $lower = 3, $numeric = 3, $other = 3 ) {
+
+    $passOrder = Array(); $passWord = "";
+
+    for ( $i = 0; $i < $upper; $i++ ) { $passOrder[] = chr ( rand ( 65, 90 ) ); }
+
+    for ( $i = 0; $i < $lower; $i++ ) { $passOrder[] = chr ( rand ( 97, 122 ) ); }
+
+    for ( $i = 0; $i < $numeric; $i++) { $passOrder[] = chr ( rand ( 48, 57 ) ); }
+
+	  for ( $i = 0; $i < $other; $i++ ) { $passOrder[] = chr ( rand ( 33, 47 ) ); }
+
+	  shuffle ( $passOrder );
+
+	  foreach ( $passOrder as $char ) { $passWord .= $char; }
+
+		return $passWord;
+
+  }
+
+  if ( isset ( $_GET['tip_r'] ) && !empty ( $_GET['tip_r'] ) ) {
+
+    list ( $tips, $num ) = explode ( "_", $_GET['tip_r'] );
+
+    switch ( $num ) {                      
+      case '1': $tips = str_replace ( "1", "12", $tips ); break;
+      case '2': $tips = str_replace ( "1", "6", $tips ); break;
+      case '3': $tips = str_replace ( "1", "4", $tips ); break;
+      case '4': $tips = str_replace ( "1", "3", $tips ); break;    
+    }
+
+    $rp = explode ( "|", $tips );
+    echo randPass ( $rp[0], $rp[1], $rp[2], $rp[3] );
+
+  } else { echo randPass(); }
---
+++ script/core.js
@@ -0,0 +1,66 @@
+function empty( mixed_var ) {
+  return ( mixed_var === "" || mixed_var === 0 || mixed_var === "0" || mixed_var === null || mixed_var === false || mixed_var === undefined || mixed_var.length === 0 );
+}
+
+jQuery.ajaxSetup({
+  cache: false
+});
+
+var chkb = new Array ( "opt1", "opt2", "opt3", "opt4" );
+
+function gen() {
+
+  var err = 0;
+  var datastr = "";
+
+  for ( var i = 0; i < chkb.length; i++ ) {
+
+    if ( !$("#" + chkb[i] + "").attr ("checked") ) {
+
+      datastr += "0|";
+      err++;
+
+    } else { datastr += "1|"; }
+
+  }
+
+  datastr = datastr.substr ( 0, ( datastr.length -1 ) );
+
+  if ( err < chkb.length ) {
+
+    datastr += "_" + ( chkb.length - err );
+
+    $.ajax({
+      type: "GET",
+      url: "./pages/get.php",
+      data: { tip_r: datastr },
+      cache: false,
+      success: function(result){
+        if ( !empty ( result ) ) {
+          $("#box_text").html ( result );
+        }
+      }
+    })
+
+  } else { alert("Selezionare almeno una tipologia!"); }
+
+}
+
+$(document).ready(function() {
+
+  for ( var i = 0; i < chkb.length; i++ ) {
+    $("#" + chkb[i] + "").attr ( "checked", "checked" );
+  }
+
+  $.ajax({
+    type: "GET",
+    url: "./pages/get.php",
+    cache: false,
+    success: function(result){
+      if ( !empty ( result ) ) {
+        $("#box_text").html ( result );
+      }
+    }
+  })
+
+});
