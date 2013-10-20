154f6349da64e31e36dd807095a8fafa71e04ec8
A35G <a35g@hackworld.it>
2013-10-20 18:28:15 +0200

    Optimized Code

--- script/core.js
+++ script/core.js
@@ -8,20 +8,21 @@
 */
 
 function empty( mixed_var ) {
-  return ( mixed_var === "" || mixed_var === 0 || mixed_var === "0" || mixed_var === null || mixed_var === false || mixed_var === undefined || mixed_var.length === 0 );
+  return ( mixed_var === '' || mixed_var === 0 || mixed_var === '0' || 
+  mixed_var === null || mixed_var === false || mixed_var === undefined || 
+  mixed_var.length === 0 );
 }
 
 jQuery.ajaxSetup({
   cache: false
 });
 
-var chkb = new Array ( "opt1", "opt2", "opt3", "opt4" );
+var chkb = new Array ( 'opt1', 'opt2', 'opt3', 'opt4' );
 
 function selAll() {
 
-  for ( var c=0; c < chkb.length; c++) {
+  for ( var c=0; c < chkb.length; c++)
     $('#'+chkb[c]+'').attr('checked', 'checked');
-  }
     
   return '1|1|1|1_4';
 
@@ -33,15 +34,20 @@ function checkOpt() {
 
   for (var i=0; i < chkb.length; i++) {
 
-    if (!$('#'+chkb[i]+'').attr('checked')) { datastr += '0|'; err++; }
-    else { datastr += '1|'; }
+    if (!$('#'+chkb[i]+'').attr('checked')) {
+      datastr += '0|'; err++;
+    } else {
+      datastr += '1|';
+    }
 
   }
   
   datastr = datastr.substr(0, (datastr.length -1));
 
-  if (err == chkb.length) { datastr = selAll(); }
-  else { datastr += "_" + ( chkb.length - err ); }
+  if (err == chkb.length)
+    datastr = selAll();
+  else
+    datastr += "_" + ( chkb.length - err );
   
   return datastr;
 
@@ -59,7 +65,8 @@ function gen() {
       data: {tip_r: datastr},
       cache: false,
       success: function(result){
-        if (!empty(result)) { $('#box_text').html(result); }
+        if (!empty(result))
+          $('#box_text').html(result);
       }
     })
     
@@ -68,7 +75,5 @@ function gen() {
 }
 
 $(document).ready(function() {
-
   gen();
-
 });
